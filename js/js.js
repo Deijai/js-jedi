744807a452df9d6ae8b2a29c644f22ccf073fe59
deijai <djairn18@gmail.com>
2019-02-15 22:22:52 -0200

    Signed-off-by: deijai <djairn18@gmail.com>gdsgd

---
+++ js/js.js
@@ -0,0 +1,33 @@
+//funções
+/*function exibirNome() {
+    let nome = 'Deijai';
+    let sobreNome = 'Miranda';
+
+    console.log('Nome completo: ' + nome + ' ' + sobreNome);
+}
+
+function clicarButton() {
+    alert('clicou no botao!');
+}
+
+
+//declarar obj
+
+let objPessoa = {
+    nome: 'Deijai Miranda',
+    formacao: 'Bacharel em Sistema de Informação',
+    mostraDados: function () {
+        console.log('Dados: ' + objPessoa.nome + ' ' + objPessoa.formacao);
+    }
+}
+
+let p = document.getElementById('p');
+p.append(objPessoa.nome);
+
+objPessoa.mostraDados();
+*/
+
+//execução sincrona
+for (let index = 0; index < 10; index++) {
+    console.log(index);
+}
